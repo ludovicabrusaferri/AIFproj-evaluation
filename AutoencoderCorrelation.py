6998db57c7991e28f8138784128857a27746c1aa
Ludovica <loginvarious12@gmail.com>
2024-06-20 15:57:57 +0100

    image

--- AutoencoderCorrelation.py
+++ AutoencoderCorrelation.py
@@ -4,187 +4,180 @@ import os
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from sklearn.preprocessing import StandardScaler
-from mpl_toolkits.mplot3d import Axes3D
+import nibabel as nib
 
 subject = 24
 num_timestamps = 26
-flattened_signals = []
 
-plotsave = '/Users/e410377/Desktop/AIFproj-evaluation/OUT/ReformattedOriginalDataKCL_ALL/figures'
-
-# Load the true values
-true_value_file_path = f'/Users/e410377/Desktop/Ludo/AlexLudo/ReformattedOriginalDataKCL_ALL/patient_data/metabolite_corrected_signal_data/{subject}.txt'
-metuncor_path = f'/Users/e410377/Desktop/Ludo/AlexLudo/ReformattedOriginalDataKCL_ALL/patient_data/signal_data/{subject}.txt'
-
-if os.path.exists(true_value_file_path):
-    true_values = np.loadtxt(true_value_file_path)
-    print("Loaded true values")
-else:
-    raise FileNotFoundError(f"True value file path does not exist: {true_value_file_path}")
-
-if os.path.exists(metuncor_path):
-    uncor_values = np.loadtxt(metuncor_path)
-    print("Loaded uncorrected values")
-else:
-    raise FileNotFoundError(f"Uncorrected value file path does not exist: {metuncor_path}")
-
-original_shapes = []
-
-for timestamp in range(num_timestamps):
-    signal_mean_path = f'/Users/e410377/Desktop/AIFproj-evaluation/OUT/ReformattedOriginalDataKCL_ALL/autoencoder/{subject}_0/{timestamp}_latent_mean.pkl'
-    signal_std_path = f'/Users/e410377/Desktop/AIFproj-evaluation/OUT/ReformattedOriginalDataKCL_ALL/autoencoder/{subject}_0/{timestamp}_latent_stddev.pkl'
-    image_path = f'/Users/e410377/Desktop/Ludo/AlexLudo/ReformattedOriginalDataKCL_ALL/patient_data/image_data/{subject}/{subject}_000{timestamp}.nii.gz'
-
-    # Check if the file paths exist
-    if os.path.exists(signal_mean_path) and os.path.exists(signal_std_path):
-        with open(signal_mean_path, 'rb') as file:
-            signal_mean = pickle.load(file)
-
-        with open(signal_std_path, 'rb') as file:
-            signal_std = pickle.load(file)
-
-        signal_mean = signal_mean.numpy()
-        signal_std = signal_std.numpy()
-        print(f"Loaded data for timestamp {timestamp}")
-
-        signal_samples = []
-        for i in range(int(np.max([np.ceil(np.max(signal_std) * 32.0), 1.0]))):
-            signal_samples.append(np.random.normal(loc=signal_mean, scale=signal_std))
-
-        signal = np.mean(signal_samples, axis=0)
-        signal = np.squeeze(signal)
-
-        # Capture the original shape before flattening
-        original_shapes.append(signal.shape)
-
-        # Flatten the array
-        flattened_signal = signal.flatten()
-        flattened_signals.append(flattened_signal)
+def load_data(true_value_file_path, metuncor_path):
+    if os.path.exists(true_value_file_path):
+        true_values = np.loadtxt(true_value_file_path)
+        print("Loaded true values")
     else:
-        print(f"File paths for timestamp {timestamp} do not exist.")
+        raise FileNotFoundError(f"True value file path does not exist: {true_value_file_path}")
 
-print("Finished loading and processing all timestamps")
+    if os.path.exists(metuncor_path):
+        uncor_values = np.loadtxt(metuncor_path)
+        print("Loaded uncorrected values")
+    else:
+        raise FileNotFoundError(f"Uncorrected value file path does not exist: {metuncor_path}")
 
-# Convert the list to a 26xN matrix
-flattened_signals_matrix = np.array(flattened_signals)
-print("Converted flattened signals to a matrix")
+    return true_values, uncor_values
 
-# Normalize flattened true signal
-scaler_true = StandardScaler()
-true_values_normalized = np.reshape(scaler_true.fit_transform(np.reshape(true_values, (-1, 1))), true_values.shape)
-print("Normalized true values")
+def load_and_process_signals(subject, num_timestamps):
+    flattened_signals = []
+    original_shapes = []
+    workdir = '/Users/e410377/Desktop/AIFproj-evaluation/OUT/ReformattedOriginalDataKCL_ALL/autoencoder'
+    for timestamp in range(num_timestamps):
+        signal_mean_path = f'{workdir}/{subject}_0/{timestamp}_latent_mean.pkl'
+        signal_std_path = f'{workdir}/{subject}_0/{timestamp}_latent_stddev.pkl'
 
-# Normalize uncorrected values
-scaler_uncor = StandardScaler()
-uncor_values_normalized = np.reshape(scaler_uncor.fit_transform(np.reshape(uncor_values, (-1, 1))), uncor_values.shape)
-print("Normalized uncorrected values")
+        if os.path.exists(signal_mean_path) and os.path.exists(signal_std_path):
+            with open(signal_mean_path, 'rb') as file:
+                signal_mean = pickle.load(file)
 
-# Normalize flattened signals matrix
-scaler_flattened = StandardScaler()
-flattened_signals_matrix_normalized = np.reshape(scaler_flattened.fit_transform(np.reshape(flattened_signals_matrix, (-1, flattened_signals_matrix.shape[1]))), flattened_signals_matrix.shape)
-print("Normalized flattened signals matrix")
+            with open(signal_std_path, 'rb') as file:
+                signal_std = pickle.load(file)
 
-# Plot the signals in a 5x6 grid
-fig, axs = plt.subplots(5, 6, figsize=(20, 20))
+            signal_mean = signal_mean.numpy()
+            signal_std = signal_std.numpy()
+            print(f"Loaded data for timestamp {timestamp}")
 
-for i in range(5):
-    for j in range(6):
-        timestamp_index = i * 6 + j
-        if timestamp_index < num_timestamps:
-            axs[i, j].plot(flattened_signals_matrix_normalized[timestamp_index])
-            axs[i, j].set_title(f'Timestamp {timestamp_index}')
-            axs[i, j].set_xlabel('Index')
-            axs[i, j].set_ylabel('Value')
+            signal_samples = []
+            for i in range(int(np.max([np.ceil(np.max(signal_std) * 32.0), 1.0]))):
+                signal_samples.append(np.random.normal(loc=signal_mean, scale=signal_std))
 
-plt.tight_layout()
+            signal = np.mean(signal_samples, axis=0)
+            signal = np.squeeze(signal)
 
-# Save the figure as a jpg file
-fig_path = os.path.join(plotsave, f'subject_{subject}_flattened_signals_plot.jpg')
-plt.savefig(fig_path, format='jpg')
-plt.close()
-print("Saved flattened signals plot")
+            original_shapes.append(signal.shape)
+            flattened_signal = signal.flatten()
+            flattened_signals.append(flattened_signal)
+        else:
+            print(f"File paths for timestamp {timestamp} do not exist.")
 
-# Flatten the normalized true values
-flattened_true_values_normalized = true_values_normalized.flatten()
-print("Flattened true values")
+    return np.array(flattened_signals), original_shapes
 
-# Flatten the normalized uncorrected values
-flattened_uncor_values_normalized = uncor_values_normalized.flatten()
-print("Flattened uncorrected values")
+def load_and_process_images(subject, num_timestamps):
+    flattened_images = []
+    workdir = '/Users/e410377/Desktop/Ludo/AlexLudo/ReformattedOriginalDataKCL_ALL/patient_data/image_data'
+    for timestamp in range(num_timestamps):
+        image_path = f'{workdir}/{subject}/{subject}_00{timestamp:02d}.nii.gz'
 
-# Calculate the correlation for each element across the timestamps
-correlations = []
-for i in range(flattened_signals_matrix_normalized.shape[1]):
-    correlation = np.corrcoef(flattened_signals_matrix_normalized[:, i], flattened_true_values_normalized)[0, 1]
-    correlations.append((i, correlation))  # Store index and correlation
-    if i % 1000 == 0:
-        print(f"Calculated correlation for element {i}")
+        if os.path.exists(image_path):
+            img = nib.load(image_path)
+            img_data = img.get_fdata()
+            img_flattened = img_data.flatten()
+            flattened_images.append(img_flattened)
+            print(f"Loaded and flattened image data for timestamp {timestamp}")
+        else:
+            print(f"Image file path for timestamp {timestamp} does not exist.")
 
-# Sort correlations in descending order
-correlations_sorted = sorted(correlations, key=lambda x: x[1], reverse=True)
-print("Sorted correlations")
+    return np.array(flattened_images)
 
-# Extract sorted indices and values
-sorted_indices = [x[0] for x in correlations_sorted]
-sorted_correlations = [x[1] for x in correlations_sorted]
+def normalize_data(data):
+    scaler = StandardScaler()
+    return np.reshape(scaler.fit_transform(np.reshape(data, (-1, data.shape[1]))), data.shape)
 
-# Convert correlations to a numpy array
-sorted_correlations = np.array(sorted_correlations)
+def calculate_and_plot_correlations(data_matrix, flattened_true_values_normalized, flattened_uncor_values_normalized, data_type, plotsave):
+    # Calculate the correlation for each element across the timestamps
+    correlations = []
+    for i in range(data_matrix.shape[1]):
+        correlation = np.corrcoef(data_matrix[:, i], flattened_true_values_normalized)[0, 1]
+        correlations.append((i, correlation))  # Store index and correlation
+        if i % 1000 == 0:
+            print(f"Calculated correlation for {data_type} element {i}")
 
-# Plot the best score vs true and uncorrected, the correlation, original true vs uncorrected vs best score before rescaling,
-# and the rescaling parameters (mean and scale) for the best signal
+    # Sort correlations in descending order
+    correlations_sorted = sorted(correlations, key=lambda x: x[1], reverse=True)
+    print(f"Sorted {data_type} correlations")
 
-fig, axs = plt.subplots(2, 1, figsize=(20, 32))
+    # Extract sorted indices and values
+    sorted_indices = [x[0] for x in correlations_sorted]
+    sorted_correlations = [x[1] for x in correlations_sorted]
 
-best_index = sorted_indices[0]
-best_signal_normalized = flattened_signals_matrix_normalized[:, best_index]
-best_signal_original = flattened_signals_matrix[:, best_index]
+    # Plot the best score vs true and uncorrected, the correlation, original true vs uncorrected vs best score before rescaling,
+    # and the rescaling parameters (mean and scale) for the best signal
 
-# Plot the best signal vs the true signal and uncorrected signal (normalized)
-axs[0].plot(best_signal_normalized, label='Best Signal')
-axs[0].plot(flattened_true_values_normalized, label='True Signal', linestyle='--')
-axs[0].plot(flattened_uncor_values_normalized, label='Uncorrected Signal', color='black', linestyle='-.')
-axs[0].set_title(f'Subject {subject} - Best Signal vs True and Uncorrected Signal (Normalized)')
-axs[0].set_xlabel('Index')
-axs[0].set_ylabel('Value')
-axs[0].legend()
+    fig, axs = plt.subplots(2, 1, figsize=(20, 32))
 
-# Create a scatter plot with a regression line (normalized)
-slope, intercept, r_value, p_value, std_err = linregress(flattened_true_values_normalized, best_signal_normalized)
-line = slope * flattened_true_values_normalized + intercept
-axs[1].scatter(flattened_true_values_normalized, best_signal_normalized, label='Data Points')
-axs[1].plot(flattened_true_values_normalized, line, color='red', label=f'Regression Line (r={r_value:.2f})')
-axs[1].set_title(f'Subject {subject} - Scatter Plot with Regression Line (Normalized)')
-axs[1].set_xlabel('True Signal')
-axs[1].set_ylabel('Best Signal')
-axs[1].legend()
+    best_index = sorted_indices[0]
+    best_signal_normalized = data_matrix[:, best_index]
 
+    # Plot the best signal vs the true signal and uncorrected signal (normalized)
+    axs[0].plot(best_signal_normalized, label=f'Best {data_type.capitalize()} Signal')
+    axs[0].plot(flattened_true_values_normalized, label='True Signal', linestyle='--')
+    axs[0].plot(flattened_uncor_values_normalized, label='Uncorrected Signal', color='black', linestyle='-.')
+    axs[0].set_title(f'Subject {subject} - Best {data_type.capitalize()} Signal vs True and Uncorrected Signal (Normalized)')
+    axs[0].set_xlabel('Index')
+    axs[0].set_ylabel('Value')
+    axs[0].legend()
 
-# Save the plot as a jpg file
-best_signal_plot_path = os.path.join(plotsave, f'subject_{subject}_best_signals_vs_true_uncorrected_rescaling.jpg')
-plt.savefig(best_signal_plot_path, format='jpg')
-plt.close()
-print("Saved best signals vs true and uncorrected signal plot with rescaling parameters")
+    # Create a scatter plot with a regression line (normalized)
+    slope, intercept, r_value, p_value, std_err = linregress(flattened_true_values_normalized, best_signal_normalized)
+    line = slope * flattened_true_values_normalized + intercept
+    axs[1].scatter(flattened_true_values_normalized, best_signal_normalized, label='Data Points')
+    axs[1].plot(flattened_true_values_normalized, line, color='red', label=f'Regression Line (r={r_value:.2f})')
+    axs[1].set_title(f'Subject {subject} - Scatter Plot with Regression Line (Normalized)')
+    axs[1].set_xlabel('True Signal')
+    axs[1].set_ylabel(f'Best {data_type.capitalize()} Signal')
+    axs[1].legend()
 
-# Determine the 4D coordinates of the top 20 best indices
-#top_indices = sorted_indices[:50]
-#coordinates = [np.unravel_index(idx, original_shapes[0]) for idx in top_indices]
+    # Save the plot as a jpg file
+    plot_path = os.path.join(plotsave, f'subject_{subject}_best_{data_type}_signals_vs_true_uncorrected_rescaling.jpg')
+    plt.savefig(plot_path, format='jpg')
+    plt.close()
+    print(f"Saved best {data_type} signals vs true and uncorrected signal plot with rescaling parameters")
 
-# Plot the 3D scatter plot of the top 20 best indices with color representing the fourth dimension
-#fig = plt.figure(figsize=(10, 8))
-#ax = fig.add_subplot(111, projection='3d')
-#x, y, z, p = zip(*coordinates)
-#sc = ax.scatter(x, y, z, c=p, cmap='viridis', s=100)
-#ax.set_title(f'Subject {subject} - Top 20 Best Indices in 3D')
-#ax.set_xlabel('X Coordinate')
-#ax.set_ylabel('Y Coordinate')
-#ax.set_zlabel('Z Coordinate')
-#cbar = plt.colorbar(sc, ax=ax, pad=0.1)
-#cbar.set_label('Fourth Dimension (P)')
-#ax.grid(True)
+def plot_hidden_layer_signals(flattened_signals_matrix_normalized, num_timestamps, plotsave):
+    fig, axs = plt.subplots(5, 6, figsize=(20, 20))
 
-# Save the 3D scatter plot as a jpg file
-#scatter_plot_path = os.path.join(plotsave, f'subject_{subject}_top_20_indices_3d_scatter.jpg')
-#plt.savefig(scatter_plot_path, format='jpg')
-##plt.close()
-#print("Saved 3D scatter plot of the top 20 best indices")
+    for i in range(5):
+        for j in range(6):
+            timestamp_index = i * 6 + j
+            if timestamp_index < num_timestamps:
+                axs[i, j].plot(flattened_signals_matrix_normalized[timestamp_index])
+                axs[i, j].set_title(f'Timestamp {timestamp_index}')
+                axs[i, j].set_xlabel('Index')
+                axs[i, j].set_ylabel('Value')
+
+    plt.tight_layout()
+    fig_path = os.path.join(plotsave, f'subject_{subject}_flattened_signals_plot.jpg')
+    plt.savefig(fig_path, format='jpg')
+    plt.close()
+    print("Saved flattened signals plot")
+
+def main():
+    # Define file paths
+    plotsave = '/Users/e410377/Desktop/AIFproj-evaluation/OUT/ReformattedOriginalDataKCL_ALL/figures'
+    true_value_file_path = f'/Users/e410377/Desktop/Ludo/AlexLudo/ReformattedOriginalDataKCL_ALL/patient_data/metabolite_corrected_signal_data/{subject}.txt'
+    metuncor_path = f'/Users/e410377/Desktop/Ludo/AlexLudo/ReformattedOriginalDataKCL_ALL/patient_data/signal_data/{subject}.txt'
+
+    # Load true and uncorrected values
+    true_values, uncor_values = load_data(true_value_file_path, metuncor_path)
+
+    # Load and process signals
+    flattened_signals_matrix, original_shapes = load_and_process_signals(subject, num_timestamps)
+    print("Converted flattened signals to matrix")
+
+    # Load and process images
+    flattened_images_matrix = load_and_process_images(subject, num_timestamps)
+    print("Converted flattened images to matrix")
+
+    # Normalize data
+    flattened_signals_matrix_normalized = normalize_data(flattened_signals_matrix)
+    flattened_images_matrix_normalized = normalize_data(flattened_images_matrix)
+    true_values_normalized = normalize_data(true_values.reshape(-1, 1)).flatten()
+    uncor_values_normalized = normalize_data(uncor_values.reshape(-1, 1)).flatten()
+
+    # Plot hidden layer signals
+    plot_hidden_layer_signals(flattened_signals_matrix_normalized, num_timestamps, plotsave)
+
+    # Calculate and plot correlations for latent space signals
+    calculate_and_plot_correlations(flattened_signals_matrix_normalized, true_values_normalized, uncor_values_normalized, 'latent', plotsave)
+
+    # Calculate and plot correlations for images
+    calculate_and_plot_correlations(flattened_images_matrix_normalized, true_values_normalized, uncor_values_normalized, 'image', plotsave)
+
+if __name__ == "__main__":
+    main()
